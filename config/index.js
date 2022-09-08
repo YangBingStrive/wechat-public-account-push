64820462c7ffc91b23e6a7db65ce1c23a659e3f3
YangBingStrive <95903709+YangBingStrive@users.noreply.github.com>
2022-09-08 20:01:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,17 +31,17 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "亲爱的小不点",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "otr4m6luhWpQMz8DJ-bAWCkUHV4Q",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "K210HU4QNVzuNXO9l-6modjjQc9Z2Uo_goJogiWLXJQ",
       // 所在省份
       province: "广东",
       // 所在城市
-      city: "肇庆",
+      city: "广州",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
+      openUrl: "https://s.weibo.com/top/summary/"
     }, 
     {
       name: "老婆1", 
@@ -95,7 +95,7 @@ export const config = {
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
   PROVINCE: "广东",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "广州",
 
   /** 重要节日相关 */
 
